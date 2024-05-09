cd8a01415bc2e7f09a56ee20bd06ad923519a529
Zakary <chauve.zakary@gmail.com>
2024-05-09 15:04:43 +0200

    DeleteJob done

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,8 @@ import NotFoundPage from './pages/NotFoundPage';
 import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
 
-const App = () => {
+const App = () => {JobsPage
+  // Add New Job
   const addJob = async (newJob) => {
     const res = await fetch('/api/jobs', {
       method: 'POST',
@@ -21,6 +22,14 @@ const App = () => {
     });
     return;
   };
+
+  // Delete Job
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE'
+    });
+    return;
+  }
   
   const router = createBrowserRouter(
     createRoutesFromElements( 
@@ -28,7 +37,7 @@ const App = () => {
         <Route index element={<HomePage />} /> 
         <Route path='/jobs' element={<JobsPage />}/>
         <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>}/>
-        <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader}/>
+        <Route path='/jobs/:id' element={<JobPage deleteJob={ deleteJob } />} loader={jobLoader}/>
         <Route path='*' element={<NotFoundPage />}/>
       </Route>
     )
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -2,10 +2,24 @@ import { useParams, useLoaderData } from 'react-router-dom';
 import {Link} from 'react-router-dom';
 import { FaArrowLeft } from 'react-icons/fa';
 import { FaMapMarker } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
 
-const JobPage = () => {
+const JobPage = ( { deleteJob } ) => {
+  const navigate = useNavigate();
   const {id} = useParams();
- const job = useLoaderData();
+  const job = useLoaderData();
+
+  const onDeleteClick = (jobId) => {
+    const confirm = window.confirm(
+      'Are you sure you want to delete this listing ?'
+    );
+
+    if (!confirm) return;
+
+    deleteJob(jobId);
+
+    navigate('/jobs')
+  }
 
   return (
     <>
@@ -79,14 +93,14 @@ const JobPage = () => {
                 <p className="my-2 bg-indigo-100 p-2 font-bold">{job.company.contactPhone}</p>
               </div>
 
-              {/* <!-- Manage --> */}JobsPage
+              {/* <!-- Manage --> */}
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                 <Link
                   to={`/jobs/edit/${job.id}`}
                   className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                   >Edit Job</Link>
-                <button
+                <button onClick={ () => onDeleteClick(job.id) }
                   className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >
                   Delete Job
